43548556b6e3b9ffcce29906a8c9289cbf637ae4
HoangMinh_Vu <hoangminh_vu@datahouse.com>
2022-05-23 16:53:47 +0700

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,60 @@
+export const dayOfWeek = [
+  {
+    name: "Monday",
+    value: 2,
+  },
+  {
+    name: "Tuesday",
+    value: 3,
+  },
+  {
+    name: "Wednesday",
+    value: 4,
+  },
+  {
+    name: "Thursday",
+    value: 5,
+  },
+  {
+    name: "Friday",
+    value: 6,
+  },
+];
+export const staticData = [
+  {
+    account: "PhuongTD14",
+    startWfhDate: 2,
+  },
+  {
+    account: "ThuyLTT9",
+    startWfhDate: 2,
+  },
+  {
+    account: "TinhHV",
+    startWfhDate: 2,
+  },
+  {
+    account: "AnNT25",
+    startWfhDate: 3,
+  },
+  {
+    account: "NguyenNC6",
+    startWfhDate: 3,
+  },
+  {
+    account: "NhaNM2",
+    startWfhDate: 3,
+  },
+  {
+    account: "MinhHL2",
+    startWfhDate: 4,
+  },
+  {
+    account: "MinhVH11",
+    startWfhDate: 4,
+  },
+  {
+    account: "TrinhTC1",
+    startWfhDate: 4,
+  },
+];
---
+++ src/enums/index.ts
@@ -0,0 +1,5 @@
+export enum RefWeek {
+    Week1 = 0,
+    Week2 = 1,
+    Week3 = 2
+}
